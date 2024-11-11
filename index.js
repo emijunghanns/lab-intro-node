1597389508fa7873cf7aac0a81287800b5d88843
Emilia Junghanns <emijunghanns@hotmail.com>
2024-11-11 12:44:35 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    if (this.items.length > 1) {
+      this.items.sort((a, b) => a - b);
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
 
-  avg() {}
+    let sumArr = this.items.reduce((acc, curr) => acc + curr, 0);
+    return sumArr;
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let sumArr = this.items.reduce((acc, curr) => acc + curr, 0);
+    return sumArr / this.items.length;
+  }
 }
 
 module.exports = SortedList;
